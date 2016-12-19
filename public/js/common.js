8ee0bc8fa0f8c2bfda67d8a982a9cf2ff4e30a3a
BGazsi <bgazsi94@gmail.com>
2016-12-19 09:26:20 +0100

    jquery kivetele

--- public/js/common.js
+++ public/js/common.js
@@ -1,10 +1,15 @@
-$(document).ready(function() {
-    var pageNumber = $('.main-content').data('page');
-    if(pageNumber) {
+document.addEventListener('DOMContentLoaded', function() {
+    var pageNumber = document.querySelector('.main-content').getAttribute('data-page');
+    if (pageNumber) {
         sessionStorage.setItem('pageNumber', pageNumber);
     }
 
-    $('[data-component~="back-btn"]').on('click', function() {
-        window.location = '/fetch/' + sessionStorage.getItem('pageNumber');
-    });
-});
+    var backBtn = document.querySelector('[data-component~="back-btn"]');
+
+    if(backBtn) {
+        backBtn.addEventListener('click', function () {
+            window.location = '/fetch/' + sessionStorage.getItem('pageNumber');
+        });
+    }
+
+}, false);
